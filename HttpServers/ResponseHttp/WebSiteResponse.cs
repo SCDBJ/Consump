b32454aa42e8b554ea44afb53a882c68c2c194ce
周振平 <429056015@qq.com>
2025-12-13 10:00:46 +0800

    B

--- HttpServers/ResponseHttp/WebSiteResponse.cs
+++ HttpServers/ResponseHttp/WebSiteResponse.cs
@@ -73,9 +73,9 @@ namespace HttpServers.IHttpServer
         }
         public void ModifyWebSite(string content, HttpListenerContext httpListenerContext)
         {
-            int websiteId = int.Parse(Regex.Match(content, @"\""websiteId\"":\""(?<websiteId>[\S\s]*?)\""").Groups["websiteId"].Value);
+            int WebSiteId = int.Parse(Regex.Match(content, @"\""WebSiteId\"":\""(?<WebSiteId>[\S\s]*?)\""").Groups["WebSiteId"].Value);
             string commonUse = Regex.Match(content, @"\""commonUse\"":\""(?<commonUse>[\S\s]*?)\""").Groups["commonUse"].Value;
-            int result = new ExecuteWebSite().ModifyWebSiteCommand(websiteId, commonUse);
+            int result = new ExecuteWebSite().ModifyWebSiteCommand(WebSiteId, commonUse);
             string msg = "{\"Status\":0,\"Msg\":\"设置失败\"}";
             if (result == 1)
             {
